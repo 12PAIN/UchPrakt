635ed936b96ac470f678cf6142e9f9d227a62a87
DochkinNikita <73274829+DochkinNikita@users.noreply.github.com>
2021-06-01 23:33:44 +0700

    Add files via upload

---
+++ Examples/JAVA/Example_1.java
@@ -0,0 +1,27 @@
+public class Example_1 {
+
+    public static int func(){
+        int n = 10;
+        return n;
+    }
+
+    public static void func1(){
+        System.out.println("Привет из функции!");
+    }
+
+    public static void main(String[] args) {
+
+        int u = func();
+        System.out.println(u);
+
+        func1();
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Examples/JAVA/Example_3.java
@@ -0,0 +1,13 @@
+public class Example_3 {
+
+    public static void main(String[] args) {
+        count(10);
+        System.out.println("Count complete!");
+    }
+
+    public static void count(int to) {
+        for (int i=0; i<10; i++){
+            System.out.println(i);
+        }
+    }
+}
